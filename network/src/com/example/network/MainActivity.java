708860bb3700b2e48241ea36c58bce52b4069775
fannyting <fannyting0227@gmail.com>
2014-03-27 21:01:34 +0800

    fetch

--- network/src/com/example/network/MainActivity.java
+++ network/src/com/example/network/MainActivity.java
@@ -7,49 +7,128 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+@SuppressLint("NewApi")
 public class MainActivity extends Activity {
+
+	private TextView textView;
+	private EditText editText;
+	private ProgressDialog progress;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		textView = (TextView) findViewById(R.id.textView1);
+		editText = (EditText) findViewById(R.id.editText1);
+		progress = new ProgressDialog(this);
+
+		// StrictMode.ThreadPolicy policy = new
+		// StrictMode.ThreadPolicy.Builder()
+		// .permitAll().build();
+		// StrictMode.setThreadPolicy(policy);
 	}
 
-	private TextView textview;
-	
 	public void fetch(View view) {
-		String urlStr = "http://yahoo.com";
+		String address = editText.getText().toString();
+		final String urlStr = String
+				.format("http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false",
+						address);
 
-		try {
-			URL url = new URL(urlStr);
-			URLConnection connection = url.openConnection();
+		AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
 
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(
-					connection.getInputStream()));
-
-			String result = "";
-			String line;
-			while( (line = buffer.readLine()) != null) {
-				result += line;
+			@Override
+			protected void onPreExecute() {
+				progress.setTitle("Loading");
+				progress.show();
 			}
-            
-			textview.setText(result);
 
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			@Override
+			protected String doInBackground(Void... params) {
+				try {
+					URL url = new URL(urlStr);
+					URLConnection connection = url.openConnection();
+
+					BufferedReader buffer = new BufferedReader(
+							new InputStreamReader(connection.getInputStream()));
+
+					StringBuilder stringBuilder = new StringBuilder();
+					String line;
+					while ((line = buffer.readLine()) != null) {
+						stringBuilder.append(line);
+					}
+
+					return stringBuilder.toString();
+
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(String result) {
+				textView.setText(result);
+			}
+		};
+
+		task.execute();
 	}
 
+	public void fetch2(View view) {
+		final String urlStr = "http://tw.yahoo.com/";
+
+		AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+
+			@Override
+			protected void onPreExecute() {
+			}
+
+			@Override
+			protected String doInBackground(Void... params) {
+				DefaultHttpClient httpClient = new DefaultHttpClient();
+				HttpGet get = new HttpGet(urlStr);
+
+				ResponseHandler<String> responseHandler = new BasicResponseHandler();
+				try {
+					return httpClient.execute(get, responseHandler);
+				} catch (ClientProtocolException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return "";
+			}
+
+			@Override
+			protected void onPostExecute(String result) {
+				textView.setText(result);
+			}
+		};
+
+		task.execute();
+	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
